6e90dd59dce369e5994c3632dcef33afa47640ff
oleg <olegovsei@gmail.com>
2020-06-02 20:53:24 +1000

    add test requirements to setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ install_requires = [
     'aiohttp==3.6.2',
     'uvloop==0.14.0',
     'aiohttp-jinja2==1.2.0',
+    'pytest==5.4.2',
+    'pytest-aiohttp==0.3.0',
+    'pytest-asyncio==0.12.0',
 ]
 
 
